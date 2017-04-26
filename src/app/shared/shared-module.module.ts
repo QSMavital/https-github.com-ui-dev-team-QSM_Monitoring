f69aa32fb0b75da74d268dcc43503991b9f0ebe4
Liron Boberman <lironboberman@gmail.com>
2017-04-26 13:46:13 +0300

    Budu directive

--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {HttpClientService} from "./services/http-client.service";
+import {DirSelectionDirective} from './directives/bidi/dir-selection.directive';
 
 @NgModule({
   imports: [],
-  declarations: [],
-  exports: [],
-  providers: [ HttpClientService]
+  declarations: [
+    DirSelectionDirective
+  ],
+  exports: [
+    DirSelectionDirective
+  ],
+  providers: [
+    HttpClientService
+  ]
 })
 export class SharedModuleModule {
 }

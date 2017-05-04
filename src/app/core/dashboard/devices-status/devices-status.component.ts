4723d449709e483e95778471879d9eda2cf1f423
oryt <ory.tabak@gmail.com>
2017-05-04 11:34:28 +0300

    4/5 finished devices status

--- src/app/core/dashboard/devices-status/devices-status.component.ts
+++ src/app/core/dashboard/devices-status/devices-status.component.ts
@@ -14,8 +14,9 @@ export class DevicesStatusComponent implements OnInit, OnChanges {
       maxValue: 100,
       minValue: 0,
       value: 10,
+      icon:'icon-unavailable',
       angle: 1, // The span of the gauge arc
-      lineWidth: 0.05, // The line thickness
+      lineWidth: 0.1, // The line thickness
       radiusScale: .8, // Relative radius
       limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
       limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
@@ -31,13 +32,50 @@ export class DevicesStatusComponent implements OnInit, OnChanges {
       maxValue: 100,
       minValue: 0,
       value: 10,
+      icon:'icon-fix',
       angle: 1, // The span of the gauge arc
-      lineWidth: 0.05, // The line thickness
+      lineWidth: 0.1, // The line thickness
       radiusScale: .8, // Relative radius
       limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
       limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
       colorStart: '#fa9245',   // Colors
       colorStop: '#fa9245',    // just experiment with them
+      strokeColor: '#f4f7fa',  // to see which ones work best for you
+      generateGradient: false,
+      highDpiSupport: true
+    },
+    {
+      type: 'Donut',
+      title: 'db.broken',
+      maxValue: 100,
+      minValue: 0,
+      value: 10,
+      icon:'icon-broken',
+      angle: 1, // The span of the gauge arc
+      lineWidth: 0.1, // The line thickness
+      radiusScale: .8, // Relative radius
+      limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
+      limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
+      colorStart: '#ff563e',   // Colors
+      colorStop: '#ff563e',    // just experiment with them
+      strokeColor: '#f4f7fa',  // to see which ones work best for you
+      generateGradient: false,
+      highDpiSupport: true
+    },
+    {
+      type: 'Donut',
+      title: 'db.working',
+      maxValue: 100,
+      minValue: 0,
+      value: 10,
+      icon:'icon-working',
+      angle: 1, // The span of the gauge arc
+      lineWidth: 0.1, // The line thickness
+      radiusScale: .8, // Relative radius
+      limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
+      limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
+      colorStart: '#71d36b',   // Colors
+      colorStop: '#71d36b',    // just experiment with them
       strokeColor: '#f4f7fa',  // to see which ones work best for you
       generateGradient: false,
       highDpiSupport: true
@@ -53,8 +91,10 @@ export class DevicesStatusComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     setInterval(() => {
-      this.data[0].value = this.getRandomArbitrary(1, 99);
-      this.data[1].value = this.getRandomArbitrary(1, 99);
+      this.data.forEach(i=>{
+        i.value = this.getRandomArbitrary(1, 99);
+      })
+      // this.data[1].value = this.getRandomArbitrary(1, 99);
     }, 4000)
   }
 

92ad7b140d2b983cf5edc7659c2f3091a13d48f9
oryt <ory.tabak@gmail.com>
2017-05-07 16:55:24 +0300

    7/5 applied api data to devicesStatus

--- src/app/core/dashboard/devices-status/devices-status.component.ts
+++ src/app/core/dashboard/devices-status/devices-status.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnChanges, OnInit, OnDestroy} from '@angular/core';
 import {select, NgRedux} from "@angular-redux/store";
 import {Observable} from "rxjs";
-import {isNullOrUndefined} from "util";
+import {isArray, isNullOrUndefined} from "util";
 import {IStore} from "../../../../store/index";
 import {DashboardActions} from "../../../../store/actions/dashboard-actions";
 
@@ -16,26 +16,47 @@ export class DevicesStatusComponent implements OnInit, OnDestroy {
   private data: any[] = [
     {
       type: 'Donut',
-      title: 'db.unavailable',
+      title: 'db.no_error',
       maxValue: 100,
       minValue: 0,
       value: 10,
-      icon:'icon-unavailable',
+      statusView:'good',
+      icon:'icon-working',
       angle: 1, // The span of the gauge arc
       lineWidth: 0.1, // The line thickness
       radiusScale: .9, // Relative radius
       limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
       limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
-      colorStart: '#49bbf8',   // Colors
-      colorStop: '#49bbf8',    // just experiment with them
+      colorStart: '#71d36b',   // Colors
+      colorStop: '#71d36b',    // just experiment with them
       strokeColor: '#f4f7fa',  // to see which ones work best for you
       generateGradient: false,
       highDpiSupport: true
     },
     {
       type: 'Donut',
-      title: 'db.need_to_be_fixed',
+      title: 'db.fatal',
       maxValue: 100,
+      minValue: 0,
+      value: 10,
+      statusView:'fatal',
+      icon:'icon-broken',
+      angle: 1, // The span of the gauge arc
+      lineWidth: 0.1, // The line thickness
+      radiusScale: .9, // Relative radius
+      limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
+      limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
+      colorStart: '#ff563e',   // Colors
+      colorStop: '#ff563e',    // just experiment with them
+      strokeColor: '#f4f7fa',  // to see which ones work best for you
+      generateGradient: false,
+      highDpiSupport: true
+    },
+    {
+      type: 'Donut',
+      title: 'db.attention',
+      maxValue: 100,
+      statusView:'attention',
       minValue: 0,
       value: 10,
       icon:'icon-fix',
@@ -52,36 +73,19 @@ export class DevicesStatusComponent implements OnInit, OnDestroy {
     },
     {
       type: 'Donut',
-      title: 'db.broken',
+      title: 'db.disabled',
+      statusView:'disable',
       maxValue: 100,
       minValue: 0,
       value: 10,
-      icon:'icon-broken',
+      icon:'icon-unavailable',
       angle: 1, // The span of the gauge arc
       lineWidth: 0.1, // The line thickness
       radiusScale: .9, // Relative radius
       limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
       limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
-      colorStart: '#ff563e',   // Colors
-      colorStop: '#ff563e',    // just experiment with them
-      strokeColor: '#f4f7fa',  // to see which ones work best for you
-      generateGradient: false,
-      highDpiSupport: true
-    },
-    {
-      type: 'Donut',
-      title: 'db.working',
-      maxValue: 100,
-      minValue: 0,
-      value: 10,
-      icon:'icon-working',
-      angle: 1, // The span of the gauge arc
-      lineWidth: 0.1, // The line thickness
-      radiusScale: .9, // Relative radius
-      limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
-      limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
-      colorStart: '#71d36b',   // Colors
-      colorStop: '#71d36b',    // just experiment with them
+      colorStart: '#49bbf8',   // Colors
+      colorStop: '#49bbf8',    // just experiment with them
       strokeColor: '#f4f7fa',  // to see which ones work best for you
       generateGradient: false,
       highDpiSupport: true
@@ -90,7 +94,6 @@ export class DevicesStatusComponent implements OnInit, OnDestroy {
 
   constructor(private ngRedux: NgRedux<IStore>) {
     this.ngRedux.dispatch({type:DashboardActions.WIDGET_GET_DEVICE_STATUS});
-
   }
 
   ngOnInit() {
@@ -105,8 +108,16 @@ export class DevicesStatusComponent implements OnInit, OnDestroy {
   subscribe(){
     this._devicesStatus = this.devicesStatus$.subscribe((state)=>{
       if(!isNullOrUndefined(state)){
-        // this.data = state;
         console.log('devicesStatus--->>>>>>>>',state);
+        if(isArray(state)) {
+          state.forEach((i)=>{
+            this.data.forEach(d=>{
+              if(i.statusView.toLowerCase() === d.statusView.toLowerCase()) {
+                d.value = i.precents;
+              }
+            })
+          });
+        }
       }
     })
 

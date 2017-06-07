16f2d793229acd2fe50e720dc4b2c52ab066d9bf
Nir_Zigdon <zigdonnir@gmail.com>
2017-06-07 15:51:46 +0300

    hsm status

---
+++ src/app/shared/components/add-new-hsm/add-new-hsm.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'ui-add-new-hsm',
+  templateUrl: './add-new-hsm.component.html',
+  styleUrls: ['./add-new-hsm.component.scss']
+})
+export class AddNewHsmComponent implements OnInit {
+  @Input() get show() {
+    return this.showValue;
+  }
+  @Output() showChange = new EventEmitter();
+
+  set show(val) {
+    this.showValue = val;
+  }
+
+  private showValue = false;
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+}
